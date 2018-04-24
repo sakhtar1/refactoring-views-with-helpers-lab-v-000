477a5156a3d1ae5e14842365e88c8e3f982bced6
Sharmina Akhtar <sakht318@gmail.com>
2018-04-24 10:59:15 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
     if song.artist == " "
       link_to "Add Artist", edit_song_path(song)
     else
-      link_to song.artist, artist_path(song.artist)
+      link_to song.artist.name, artist_path(song.artist)
     end
   end
 end
